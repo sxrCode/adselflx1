b4d0b0408691ed0efda967cd8c1444aeff4e4cde
sunxiran <maricle01@163.com>
2017-11-24 21:13:45 +0800

    add ScrollActivity

---
+++ mainmodule/src/main/java/com/sxr/com/mainmodule/fragment/ScrollFragment.java
@@ -0,0 +1,24 @@
+package com.sxr.com.mainmodule.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sxr.com.mainmodule.R;
+
+/**
+ * Created by Administrator on 2017/11/24.
+ */
+
+public class ScrollFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_scroll, container, false);
+        return view;
+    }
+}
