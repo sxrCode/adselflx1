86330fe8cebbb65100843bd3ffebd88c2ac75c72
sunxiran <maricle01@163.com>
2017-12-28 17:15:18 +0800

    feature: 添加了一些资源使用的例子

--- mainmodule/src/main/java/com/sxr/com/mainmodule/MainActivity.java
+++ mainmodule/src/main/java/com/sxr/com/mainmodule/MainActivity.java
@@ -3,13 +3,14 @@ package com.sxr.com.mainmodule;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.sxr.com.mainmodule.activity.AnimationTestActivity;
-import com.sxr.com.mainmodule.activity.CircleActivity;
+import com.sxr.com.mainmodule.activity.CrimeListActivity;
 import com.sxr.com.mainmodule.swipemenu.SwipeMainActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,13 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void startOnFormal() {
         setContentView(R.layout.activity_main);
-        Log.e("SwipeMainActivity", "startOnFormal");
         mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, SwipeMainActivity.class);
+                intent.setClass(MainActivity.this, CrimeListActivity.class);
                 startActivity(intent);
             }
         });
@@ -48,6 +48,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
--- mainmodule/src/main/java/com/sxr/com/mainmodule/fragment/AnimationTestFragment.java
+++ mainmodule/src/main/java/com/sxr/com/mainmodule/fragment/AnimationTestFragment.java
@@ -1,15 +1,19 @@
 package com.sxr.com.mainmodule.fragment;
 
-import android.animation.Animator;
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
-import android.animation.ValueAnimator;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.sxr.com.mainmodule.R;
 
@@ -17,17 +21,53 @@ import com.sxr.com.mainmodule.R;
 public class AnimationTestFragment extends Fragment {
 
     private View mTestView;
+    private View mLevelView;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_animation_test, container, false);
         mTestView = view.findViewById(R.id.anim_test_view);
-        AnimatorSet anim = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(), R.animator.alpha_fade);
-        anim.setTarget(mTestView);
-        anim.start();
+        Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.view_animation_test);
+        mTestView.startAnimation(animation);
+
+        mLevelView = view.findViewById(R.id.level_test_view);
+        Drawable background = mLevelView.getBackground();
+        background.setLevel(background.getLevel() + 1000);
+
+        registerForContextMenu(mLevelView);
+        registerForContextMenu(mTestView);
+
+        mLevelView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(getActivity(), v);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.menu_main, popup.getMenu());
+                popup.show();
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(getActivity(), "click " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+            }
+        });
         return view;
     }
 
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(getActivity(), "select " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
+    }
 }
