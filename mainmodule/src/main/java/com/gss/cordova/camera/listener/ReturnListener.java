743673bfd35228b906070683cc0371db7a9fafb3
sunxiran <maricle01@163.com>
2018-02-27 18:39:50 +0800

    delete

--- mainmodule/src/main/java/com/gss/cordova/camera/listener/ReturnListener.java
+++
@@ -1,9 +0,0 @@
-package com.gss.cordova.camera.listener;
-
-/**
- * ===================================== 作 者: 陈嘉桐 版 本：1.1.4 创建日期：2017/4/26 描 述：
- * =====================================
- */
-public interface ReturnListener {
-	void onReturn();
-}
--- mainmodule/src/main/java/com/sxr/com/mainmodule/activity/CameraActivity.java
+++
@@ -1,8 +0,0 @@
-package com.sxr.com.mainmodule.activity;
-
-/**
- * Created by Administrator on 2018/2/12.
- */
-
-public class CameraActivity extends SingleFragmentActivity {
-}
