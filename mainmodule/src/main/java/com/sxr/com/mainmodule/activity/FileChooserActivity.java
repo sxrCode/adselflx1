b103da749f7f3c38a5c5f55bc81b4bb044513239
sunxiran <maricle01@163.com>
2018-03-13 18:13:22 +0800

    add file picker

---
+++ mainmodule/src/main/java/com/sxr/com/mainmodule/activity/FileChooserActivity.java
@@ -0,0 +1,8 @@
+package com.sxr.com.mainmodule.activity;
+
+/**
+ * Created by Administrator on 2018/3/13.
+ */
+
+public class FileChooserActivity extends SingleFragmentActivity {
+}
---
+++ mainmodule/src/main/java/com/sxr/com/mainmodule/fragment/FileChooserFragment.java
@@ -0,0 +1,10 @@
+package com.sxr.com.mainmodule.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2018/3/13.
+ */
+
+public class FileChooserFragment extends Fragment {
+}
